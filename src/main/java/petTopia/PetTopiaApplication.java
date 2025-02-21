a1ca415f35227b4fbe1c69eb4de2338c1a982bfe
Melody <melodyshan0504@gmail.com>
2025-02-21 09:44:23 +0800

    第一次提交 專案初始化

---
+++ src/main/java/petTopia/PetTopiaApplication.java
@@ -0,0 +1,13 @@
+package petTopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTopiaApplication.class, args);
+	}
+
+}
