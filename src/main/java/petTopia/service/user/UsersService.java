e05f242fa8f054a0ff6b76da979ae5c2bdd76cf5
akechi <akechitama34@gmail.com>
2025-03-19 10:07:38 +0800

    移除後端檔案

--- src/main/java/petTopia/service/user/UsersService.java
+++
@@ -1,7 +0,0 @@
-package petTopia.service.user;
-
-import petTopia.model.user.Users;
-
-public interface UsersService {
-    void bindOAuth2Account(Integer userId, Users.Provider provider);
-} 
---
+++ src/router/RouterVendor.js
@@ -0,0 +1,13 @@
+// RouterVendor.js
+// import ShopIndex from '@/views/shop/ShopIndex.vue';
+// import ShopCheckout from '@/views/shop/ShopCheckout.vue';
+// import ShopOrderHistory from '@/views/shop/ShopOrderHistory.vue';
+// import ShopOrderDetail from '@/views/shop/ShopOrderDetail.vue';
+// import VendorDashboard from '@/views/vendor/VendorDashboard.vue';
+// import VendorProducts from '@/views/vendor/VendorProducts.vue';
+// import VendorOrders from '@/views/vendor/VendorOrders.vue';
+// import VendorProfile from '@/views/vendor/VendorProfile.vue';
+// import VendorAnalytics from '@/views/vendor/VendorAnalytics.vue';
+
+export default [];  // 導出空陣列
+  
